8ffc16945c02c5e2b14ee9440c9e9c30813e6d2b
shugarry <114780801+Shugarry@users.noreply.github.com>
2025-04-20 17:17:06 +0200

    slight description changes

--- nvim/lua/vimopts.lua
+++ nvim/lua/vimopts.lua
@@ -35,26 +35,23 @@ vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 
 -- Remaps
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move text in visual mode
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move text up in visual mode' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move text up in visual mode' })
 
-vim.keymap.set("n", "J", "mzJ`z") -- everything in the middle
+vim.keymap.set("n", "J", "mzJ`z") -- Stay in middle while moving through file
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste selection into void' })
 
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]]) -- paste and keep paste buffer
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = 'Yank selection into clipboard' })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Yank line into clipboard' }) 
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- yank into clipboard
-vim.keymap.set("n", "<leader>Y", [["+Y]]) 
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]]) -- delete into void
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = 'Delete into void' })
 
 vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- use later
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- search and replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Search and replace' }) -- search and replace
