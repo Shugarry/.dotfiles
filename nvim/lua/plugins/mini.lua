5135ef67780705cebac5f9650d81448521260dca
shugarry <114780801+Shugarry@users.noreply.github.com>
2025-06-06 19:19:00 +0200

    added autopairs back (lol), removed neotree

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -22,6 +22,7 @@ return
 			config = function()
 				require("mini.ai").setup({ n_lines = 500 })
 				require("mini.surround").setup()
+				require("mini.pairs").setup()
 				require("mini.sessions").setup({
 					autoread = false,
 					autowrite = false,
