dc71d7af7e6f401845ab44f5fe6c4639ce93d57c
shugarry <114780801+Shugarry@users.noreply.github.com>
2025-06-06 19:16:47 +0200

    removed autopairs, was getting annoyed

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -22,7 +22,6 @@ return
 			config = function()
 				require("mini.ai").setup({ n_lines = 500 })
 				require("mini.surround").setup()
-				require("mini.pairs").setup({})
 				require("mini.sessions").setup({
 					autoread = false,
 					autowrite = false,
